aaf5e5d548f2ff6772467a2237a98d938276213b
Keungeun LEE <keulee42@gmail.com>
2021-06-04 23:54:38 +0200

    commit

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -4,8 +4,12 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include <stdio.h>
+# include <stdio.h>
 
 # include "../libft/libft.h"
+
+typedef struct s_info {
+	int	i;
+}				t_info;
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -12,7 +12,6 @@ int		main(int ac, char **av)
 		while (i < ac)
 		{
 			printf("%s\n", av[i]);
-			
 			i++;
 		}
 	}
