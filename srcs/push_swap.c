fa8e69b0a63c5133a6ab51afa7ce4c09768a0299
Keungeun LEE <keulee42@gmail.com>
2021-06-06 16:01:12 +0200

    code modified, encounter with free problem

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -5,29 +5,33 @@ int		main(int ac, char **av)
 	t_info info;
 	int i;
 
-	i = 1;
+	i = 2;
 	info.string = NULL;
 	info.av_with_space = NULL;
 	if (ac < 2)
 		ft_exit_msg("ERROR: Argument not enough");
 	else
 	{
+		info.string = ft_strjoin(av[1], " ");
 		while (i < ac)
 		{
 			// printf("av: %s\n", av[i]);
 			info.av_with_space = ft_strjoin(av[i], " ");
-			printf("%s\n", info.av_with_space);
-			info.string = ft_strjoin(info.string, info.av_with_space);
-			if (!info.string)
-			{
-				info.string = info.av_with_space;
-				printf("str: %s\n", info.string);
-				// free(info.av_with_space);
-			}
-			// free(info.av_with_space);
+			info.string = ft_strjoin(info.string, info.av_with_space);	
+			// if (!info.string)
+			// {
+			// 	info.string = info.av_with_space;
+			// 	// printf("str: %s\n", info.string);
+			// }
+			// if (info.string && info.av_with_space)
+			// {
+			// 	free(info.av_with_space);
+			// 	info.av_with_space = NULL;
+			// }
 			i++;
 		}
 	}
 	printf("%s\n", info.string);
+	free(info.string);
 	return (0);
 }
