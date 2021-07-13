8a56ebac6b4d18f8ab5c58fbca8a95d18062a5c3
Keungeun LEE <keulee42@gmail.com>
2021-07-13 02:52:16 +0200

    argument rb questioned

--- srcs/p01_sort_stack.c
+++ srcs/p01_sort_stack.c
@@ -63,44 +63,42 @@ void	sort_a_to_b(int size, t_node **a, t_node **b)
 	printf("pb: %d\n", call.pb);
 	// print_node_a(*a);
 	// print_node_b(*b);
-	// if (call.ra > call.rb)
-	// {
+	if (call.ra > call.rb)
+	{
 		i = call.rb;
 		while (0 < i)
 		{
-			// rotate_rrr(a, b, AB);
-			rev_rotate_ab(b, B);
+			rotate_rrr(a, b, AB);
+			// rev_rotate_ab(b, B);
 			i--;
 		}
-		// while (i < call.ra - call.rb)
-		// {
-			// rotate_ab(a, A);
-			// rev_rotate_ab(a, A);
-			// i++;
-		// }
-	// }
-	// else
-	// {
-		// i = call.ra;
-		// while (i)
-		// {
-			// rotate_rrr(a, b, AB);
-			// i--;
-		// }
-		// while (i < call.rb - call.ra)
-		// {
-		// 	rotate_ab(b, B);
-		//	rev_rotate_ab(b, B);
-		// 	i++;
-		// }
-	// }
+		while (i < call.ra - call.rb)
+		{
+			rev_rotate_ab(a, A);
+			i++;
+		}
+	}
+	else
+	{
+		i = call.ra;
+		while (i)
+		{
+			rotate_rrr(a, b, AB);
+			i--;
+		}
+		while (i < call.rb - call.ra)
+		{
+			rev_rotate_ab(b, B);
+			i++;
+		}
+	}
 	printf("-- operating a to b  --\n");
 	print_node_a(*a);
 	print_node_b(*b);
 	printf("-----------------------\n");
 	sort_a_to_b(call.ra, a, b);
-	// sort_b_to_a(call.rb, a, b);
-	// sort_b_to_a(call.pb - call.rb, a, b);
+	sort_b_to_a(call.rb, a, b);
+	sort_b_to_a(call.pb - call.rb, a, b);
 }
 
 void	sort_b_to_a(int size, t_node **a, t_node **b)
@@ -110,8 +108,8 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 	int 	i;
 
 	i = 0;
-	// printf("-- operating b to a in --\n");
-	// printf("size = %d\n", size);
+	printf("-- operating b to a in --\n");
+	printf("size = %d\n", size);
 	if (size < 3)
 	{
 		// push_ab(a, b, A);
@@ -134,7 +132,8 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 		{
 			push_ab(a, b, A);
 			call.pa++;
-			if ((*b)->value < call.p_big)
+			// if ((*b)->value < call.p_big)
+			if ((*a)->value < call.p_big)
 			{
 				rotate_ab(a, A);
 				call.ra++;
@@ -153,7 +152,6 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 		}
 		while (i < call.ra - call.rb)
 		{
-			// rotate_ab(a, A);
 			rev_rotate_ab(a, A);
 			i++;
 		}
@@ -166,11 +164,6 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 			rotate_rrr(a, b, AB);
 			i--;
 		}
-		// while (i < call.rb - call.ra)
-		// {
-		// 	rotate_ab(b, B);
-		// 	i++;
-		// }
 	}
 	printf("-- operating b to a --\n");
 	print_node_a(*a);
