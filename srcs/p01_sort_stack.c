dec1e5e0f6aebe152d948904e9089a58ba38c346
Keungeun LEE <keulee42@gmail.com>
2021-07-13 02:21:48 +0200

    a_to_b b_to_a algo understanding

--- srcs/p01_sort_stack.c
+++ srcs/p01_sort_stack.c
@@ -24,22 +24,21 @@ void	sort_a_to_b(int size, t_node **a, t_node **b)
 	printf("-- operating a to b in --\n");
 	printf("size = %d\n", size);
 	if (size <= 3)
-	// {
-	// 	stack_a_three(size, a, &call);
-
-	// }
+	{
+		// stack_a_three(size, a, &call);
+		// swap_ab(a, A);
 		return ;
+
+	}
 	call_init(&call);
-	// get_max_pivot(size, a, &call);
-	// get_min_pivot(size, a, &call);
 	get_pivot(size, a, &call);
-	printf("big pivot: %ld\n", call.p_big);
-	printf("small pivot: %ld\n", call.p_small);
+	printf("big pivot in a: %ld\n", call.p_big);
+	printf("small pivot in a: %ld\n", call.p_small);
 	s_tmp = size;
-	printf("ra: %d\n", call.ra);
-	printf("rb: %d\n", call.rb);
-	printf("pa: %d\n", call.pa);
-	printf("pb: %d\n", call.pb);
+	// printf("ra: %d\n", call.ra);
+	// printf("rb: %d\n", call.rb);
+	// printf("pa: %d\n", call.pa);
+	// printf("pb: %d\n", call.pb);
 	while (s_tmp--)
 	{
 		if ((*a)->value >= call.p_big)
@@ -55,8 +54,6 @@ void	sort_a_to_b(int size, t_node **a, t_node **b)
 			{
 				rotate_ab(b, B);
 				call.rb++;
-				// if ((*b)->next == NULL || (*b) == NULL)
-					// call.rb--;
 			}
 		}
 	}
@@ -64,43 +61,46 @@ void	sort_a_to_b(int size, t_node **a, t_node **b)
 	printf("rb: %d\n", call.rb);
 	printf("pa: %d\n", call.pa);
 	printf("pb: %d\n", call.pb);
-	print_node_a(*a);
-	print_node_b(*b);
-	if (call.ra > call.rb)
-	{
+	// print_node_a(*a);
+	// print_node_b(*b);
+	// if (call.ra > call.rb)
+	// {
 		i = call.rb;
 		while (0 < i)
 		{
-			rotate_rrr(a, b, AB);
+			// rotate_rrr(a, b, AB);
+			rev_rotate_ab(b, B);
 			i--;
 		}
-		while (i < call.ra - call.rb)
-		{
-			rotate_ab(a, A);
-			i++;
-		}
-	}
-	else
-	{
-		i = call.ra;
-		while (i)
-		{
-			rotate_rrr(a, b, AB);
-			i--;
-		}
+		// while (i < call.ra - call.rb)
+		// {
+			// rotate_ab(a, A);
+			// rev_rotate_ab(a, A);
+			// i++;
+		// }
+	// }
+	// else
+	// {
+		// i = call.ra;
+		// while (i)
+		// {
+			// rotate_rrr(a, b, AB);
+			// i--;
+		// }
 		// while (i < call.rb - call.ra)
 		// {
 		// 	rotate_ab(b, B);
+		//	rev_rotate_ab(b, B);
 		// 	i++;
 		// }
-	}
-	// printf("-- operating a to b  --\n");
+	// }
+	printf("-- operating a to b  --\n");
 	print_node_a(*a);
 	print_node_b(*b);
 	printf("-----------------------\n");
 	sort_a_to_b(call.ra, a, b);
-	sort_b_to_a(call.rb, a, b);
-	sort_b_to_a(call.pb - call.rb, a, b);
+	// sort_b_to_a(call.rb, a, b);
+	// sort_b_to_a(call.pb - call.rb, a, b);
 }
 
 void	sort_b_to_a(int size, t_node **a, t_node **b)
@@ -110,20 +110,19 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 	int 	i;
 
 	i = 0;
-	printf("-- operating b to a in --\n");
-	printf("size = %d\n", size);
+	// printf("-- operating b to a in --\n");
+	// printf("size = %d\n", size);
 	if (size < 3)
-	// {
+	{
+		// push_ab(a, b, A);
 		// push_ab(a, b, A);
 		return ;
-	// }
+	}
 	call_init(&call);
 	get_pivot(size, b, &call);
-	// get_max_pivot(size, b, &call);
-	// get_min_pivot(size, b, &call);
 	s_tmp = size;
-	printf("big pivot: %ld\n", call.p_big);
-	printf("small pivot: %ld\n", call.p_small);
+	printf("big pivot in b: %ld\n", call.p_big);
+	printf("small pivot in b: %ld\n", call.p_small);
 	while (s_tmp)
 	{
 		if ((*b)->value < call.p_small)
@@ -154,7 +153,8 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 		}
 		while (i < call.ra - call.rb)
 		{
-			rotate_ab(a, A);
+			// rotate_ab(a, A);
+			rev_rotate_ab(a, A);
 			i++;
 		}
 	}
@@ -172,7 +172,7 @@ void	sort_b_to_a(int size, t_node **a, t_node **b)
 		// 	i++;
 		// }
 	}
-	// printf("-- operating b to a --\n");
+	printf("-- operating b to a --\n");
 	print_node_a(*a);
 	print_node_b(*b);
 	printf("-----------------------\n");
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: keulee <keulee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:51:16 by keulee            #+#    #+#             */
-/*   Updated: 2021/07/11 12:54:51 by keulee           ###   ########.fr       */
+/*   Updated: 2021/07/12 23:40:00 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	main(int ac, char **av)
 {
 	t_info	info;
-	// int		count;
-
-	// count = 0;
+	
 	ft_init(&info);
 	if (ac == 1)
 		exit(0);
 	create_stack(&info, av);
+
 	printf("---------before---------\n");
 	print_node_a(info.stack_a);
 	print_node_b(info.stack_b);
+	
 	if (check_sorted(&info.stack_a))
 	{
 		free_stack(info.stack_a, info.stack_b);
@@ -34,9 +34,11 @@ int	main(int ac, char **av)
 	{
 		sort_a_to_b(info.listsize, &info.stack_a, &info.stack_b);
 	}
+	
 	printf("---------after---------\n");
 	print_node_a(info.stack_a);
 	print_node_b(info.stack_b);
-	// free_stack(info.stack_a, info.stack_b);
+	
+	free_stack(info.stack_a, info.stack_b);
 	return (EXIT_SUCCESS);
 }
