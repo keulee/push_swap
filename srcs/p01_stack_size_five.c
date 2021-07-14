cc60fe6092766ba3d4bd43606998b422c9cd31ed
Keungeun LEE <keulee42@gmail.com>
2021-07-14 03:38:12 +0200

    arg 5 need to think about more for algo

--- srcs/p01_stack_size_five.c
+++ srcs/p01_stack_size_five.c
@@ -2,12 +2,18 @@
 
 void	sort_max_five(int size, t_node **a, t_node **b)
 {
-	t_node *tmp;
 	long	max;
 	long	mid;
 	long	min;
 
-	tmp = *b;
+	max = get_max(size, a);
+	min = get_min(size, a);
+	if (size == 2)
+	{
+		if ((*a)->value == max)
+			swap_ab(a, A);
+		return ;
+	}
 	while (size != 3)
 	{
 		push_ab(b, a, B);
@@ -21,56 +27,53 @@ void	sort_max_five(int size, t_node **a, t_node **b)
 		max = get_max(size, a);
 		mid = get_mid_value(a);
 		min = get_min(size, a);
+		if ((*b)->next != NULL)
+		{	
+			if ((*b)->value < (*b)->next->value)
+				swap_ab(b, B);
+		}
 		// printf("max value : %ld\n", max);
 		// printf("mid value : %ld\n", mid);
 		// printf("min value : %ld\n", min);
 		// printf("b value : %d\n", (*b)->value);
-		if ((*b)->value < min)
-			push_ab(a, b, A);
-		else if (min < (*b)->value && mid > (*b)->value)
+		while (*b)
 		{
-			rotate_ab(a, A);
-			push_ab(a, b, A);
-			rev_rotate_ab(a, A);
-			// printf("here\n");
+			if ((*b)->value < min)
+				push_ab(a, b, A);
+			else if (min < (*b)->value && mid > (*b)->value)
+			{
+				rotate_ab(a, A);
+				push_ab(a, b, A);
+				rev_rotate_ab(a, A);
+				// printf("here1\n");
 
+			}
+			else if (mid < (*b)->value && max > (*b)->value)
+			{
+				rev_rotate_ab(a, A);
+				push_ab(a, b, A);
+				if (*b && mid < (*b)->value)
+				{
+					push_ab(a, b, A);
+					rotate_ab(a, A);
+				}
+				rotate_ab(a, A);
+				rotate_ab(a, A);
+				// printf("here2\n");
+			}
+			else if (max < (*b)->value)
+			{
+				push_ab(a, b, A);
+				if (*b && (*b)->value < max && (*b)->value > mid)
+				{
+					rev_rotate_ab(a, A);
+					push_ab(a, b, A);
+					rotate_ab(a, A);
+					rotate_ab(a, A);
+				}
+				rotate_ab(a, A);
+				// printf("here3\n");
+			}
 		}
-		else if (mid < (*b)->value && max > (*b)->value)
-		{
-			rev_rotate_ab(a, A);
-			push_ab(a, b, A);
-			rotate_ab(a, A);
-			rotate_ab(a, A);
-			// printf("here\n");
-		}
-		else if (max < (*b)->value)
-		{
-			push_ab(a, b, A);
-			rotate_ab(a, A);
-			// printf("here\n");
-
-		}
-		// if ((*a)->value > (*b)->value)
-		// 	push_ab(a, b, A);
-		// else if ((*b)->value > last)
-		// {
-		// 	push_ab(a, b, A);
-		// 	rotate_ab(a, A);
-		// }
-		// else
-		// {
-		// 	if ((*a)->next->value < (*b)->value)
-		// 	{
-		// 		rev_rotate_ab(a, A);
-		// 		push_ab(a, b, A);
-		// 		rotate_ab(a, A);
-		// 	}
-		// 	else
-		// 	{
-		// 		rotate_ab(a, A);
-		// 		push_ab(a, b, A);
-		// 		rev_rotate_ab(a, A);
-		// 	}
-		// }
 	}
 }
