a5844c4e4a306a4ebdf472850e2548cd4c60a575
Keungeun LEE <keulee42@gmail.com>
2021-07-31 13:36:14 +0200

    value check file deleted

--- srcs/p01_create_stack.c
+++ srcs/p01_create_stack.c
@@ -19,6 +19,33 @@ void	create_stack(t_info *info, char **av)
 	}
 }
 
+void	value_check(char *str, t_info *info)
+{
+	int			i;
+	int			neg;
+	long long	arg;
+
+	i = 0;
+	neg = 1;
+	arg = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			neg = neg * -1;
+		if (str[i] == '\0' || str[i] == '0')
+			ft_exit_msg("ERROR: Arguemnt not correct");
+	}
+	if (!ft_only_digit(&str[i]))
+		ft_exit_msg("ERROR: Argument not int");
+	while (ft_digit(str[i]))
+		arg = arg * 10 + (str[i++] - 48);
+	if (neg == -1)
+		arg *= -1;
+	if (arg > 2147483647 || arg < -2147483648)
+		ft_exit_msg("ERROR: int range not valid");
+	info->arg = arg;
+}
+
 void	insert_node(t_node **node, int value)
 {
 	t_node	*new;
