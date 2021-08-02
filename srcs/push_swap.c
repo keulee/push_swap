5925fdd40d94ea48df7ed87cdf1f18c979c046eb
Keungeun LEE <keulee42@gmail.com>
2021-08-02 23:02:03 +0200

    main comment deleted

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: keulee <keulee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:51:16 by keulee            #+#    #+#             */
-/*   Updated: 2021/08/01 19:31:03 by keulee           ###   ########.fr       */
+/*   Updated: 2021/08/02 22:55:24 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,11 @@
 int	main(int ac, char **av)
 {
 	t_info	info;
-	// int		o_size;
 	
 	ft_init(&info);
 	if (ac == 1)
 		exit(0);
 	create_stack(&info, av);
-
-	// printf("---------before---------\n");
-	// print_node_a(info.stack_a);
-	// print_node_b(info.stack_b);
-
 	if (check_sorted(&info.stack_a))
 	{
 		free_stack(info.stack_a, info.stack_b);
@@ -37,21 +31,7 @@ int	main(int ac, char **av)
 			sort_less_five(&info);
 		else
 			sort_recursive_push_swap(&info);
-	}
-
-	// o_size = info.listsize;
-	// if (!check_sorted(&info.stack_a) || o_size != get_listsize(&info.stack_a))
-	// {
-	// 	printf("KO - SORT AGAIN\n");
-	// 	printf("listsize = %d\n", get_listsize(&info.stack_a));
-	// }
-	// else
-	// 	printf("OK - SORT SUCCESS\n");
-	
-	// printf("---------after---------\n");
-	// print_node_a(info.stack_a);
-	// print_node_b(info.stack_b);
-	
+	}	
 	free_stack(info.stack_a, info.stack_b);
 	return (EXIT_SUCCESS);
 }
