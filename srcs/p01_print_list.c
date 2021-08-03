ac5fa263e3f834450ea1efee6f2768d1b1845a5e
Keungeun LEE <keulee42@gmail.com>
2021-08-03 16:27:19 +0200

    push swap srcs header added

--- srcs/p01_print_list.c
+++ srcs/p01_print_list.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   p01_print_list.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 16:22:49 by keulee            #+#    #+#             */
+/*   Updated: 2021/08/03 16:24:23 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/push_swap.h"
 
-void		print_node(t_node *node)
+void	print_node(t_node *node)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (node == NULL)
@@ -19,9 +31,9 @@ void		print_node(t_node *node)
 	printf("---------\n");
 }
 
-void		print_node_a(t_node *node)
+void	print_node_a(t_node *node)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (node == NULL)
@@ -32,18 +44,18 @@ void		print_node_a(t_node *node)
 	while (node != NULL)
 	{
 		printf("[%d] : %d\n", i, node->value);
-		// printf("- node = %p \n value = %d \n prev = %p \n next = %p \n - \n", node, node->value, node->prev, node->next);
+		// printf("- node = %p \n value = %d \n prev = %p \n next = %p \n 
+		// - \n", node, node->value, node->prev, node->next);
 		node = node->next;
 		i++;
 	}
 	printf("---------\n");
 	printf(" node a\n");
-
 }
 
-void		print_node_b(t_node *node)
+void	print_node_b(t_node *node)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (node == NULL)
@@ -59,5 +71,4 @@ void		print_node_b(t_node *node)
 	}
 	printf("---------\n");
 	printf(" node b\n");
-
 }
