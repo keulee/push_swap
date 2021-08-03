f3a7dd5847980fee10a004981547721a597c10a2
Keungeun LEE <keulee42@gmail.com>
2021-08-03 16:43:12 +0200

    bonus, libft files header added

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: keulee <keulee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 18:21:11 by keulee            #+#    #+#             */
-/*   Updated: 2021/08/01 17:46:28 by keulee           ###   ########.fr       */
+/*   Updated: 2021/08/03 16:42:20 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strdup(const char *src)
 	i = 0;
 	copy = NULL;
 	len = ft_strlen(src);
-	copy = (char *)ft_calloc((len + 1),sizeof(char));
+	copy = (char *)ft_calloc((len + 1), sizeof(char));
 	if (!copy)
 		return (NULL);
 	while (i < len)
